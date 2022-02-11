f7ff6d98920583e813432120f43d5d15c4bf4b3a
mara-olson <mara.c.olson@gmail.com>
2022-02-10 16:42:38 -0800

    Restructured data into one dictionary with sub-dicts for each melon, and then created function to print the trait & value for each melon

--- melon_info.py
+++ melon_info.py
@@ -1,24 +1,5 @@
-"""Print out all the melons in our inventory."""
 
-
-from math import remainder
-from turtle import color, window_height
-from melons import melon_names, melon_seedlessness, melon_prices
-
-
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
-
+# STARTING OFF THE ASSIGNMENT:
 # I want to create a dictionary for each melon type with keys:
 #     name
 #     seeds
@@ -29,4 +10,32 @@ for i in melon_names:
 #     average weight 
 # And then also be able to add additional keys as they arise, such as:
 #     source
+
+
+from melons import melons_dict
+
+
+# PREVIOUS INPUT, TO BE REPLACED WITH MORE EFFICIENT CODE THAT ALLOWS NEW ATTRIBUTES TO BE ADDED
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
+
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
+
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+
+
+def print_melon(dictionary):
+"""Print out all the melons in our inventory.""" 
+    for melon_name, traits in dictionary.items():
+    """For each melon in the dictionary..."""
+        for trait, value in traits.items():
+        """Access the trait & its value for each melon..."""
+            print(f'{trait}: {value}')
+
+       
     
+print_melon(melons_dict)
--- melons.py
+++ melons.py
@@ -1,23 +1,73 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+# Instead of storing names together, prices together, seedlessness together, I'd like to tie each of these attributes to their corresponding melon. Therefore I will create a dictionary for each melon with these key-value pairs
+melons_dict = {
+    'Honeydew': {
+    'name': 'Honeydew',
+    'price': 0.99,
+    'seedlessness': True,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None,
+    },
+
+    'Crenshaw': {
+        'name': 'Crenshaw',
+        'price': 2.00,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None,
+    },
+
+    'Crane': {
+        'name': 'Crane',
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None,
+    },
+
+    'Casaba': {
+        'name': 'Casaba',
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None,
+    },
+
+    'Cantaloupe': {
+        'name': 'Cantaloupe',
+        'price': 0.99,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None,
+    },
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+# melon /s_dict = Honeydew, Crenshaw, Crane, Casaba, Cantaloupe
+print(melons_dict)
